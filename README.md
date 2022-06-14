191c1016ac397a0306c85d9c7291801495eb7ca3
huabin <1184884206@qq.com>
2022-06-14 22:39:24 +0800

    选择排序

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-### 使用Intellij leetcode插件刷题的刷题代码
+## 模块介绍
+| 模块 | 用途 |
+| --- | --- |
+| src/com/huabin/leetcode/editor/cn | 使用Intellij leetcode插件刷题的刷题代码 |
+| src/com/huabin/algorithm | 算法基础 |
---
+++ src/com/huabin/algorithm/primary/Code01_SelectionSort.java
@@ -0,0 +1,37 @@
+package com.huabin.algorithm.primary;
+
+import java.util.Arrays;
+
+public class Code01_SelectionSort {
+
+    private static int[] sort(int[] array){
+        // 处理边界条件
+        if (array.length == 0 || array.length == 1) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            int minNum = array[i];
+            for(int j=i+1; j< array.length; j++){
+                if (array[j]<minNum){
+                    minNum = array[j];
+                    swap(array, i, j);
+                }
+            }
+        }
+        return array;
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 1, 4, 6, 3, 1, 8, 9, 5, 3};
+        for (int j : sort(array)) {
+            System.out.print(j + " ");
+        }
+    }
+
+}
