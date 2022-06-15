a2234efeee09946969cbbf8ec3eceecf5b5dd245
huabin <1184884206@qq.com>
2022-06-15 13:34:56 +0800

    冒泡排序

---
+++ src/com/huabin/algorithm/primary/Code02_BubbleSort.java
@@ -0,0 +1,37 @@
+package com.huabin.algorithm.primary;
+
+/**
+ * @Author huabin
+ * @DateTime 2022-06-15 12:55
+ * @Desc 冒泡排序
+ */
+public class Code02_BubbleSort {
+
+    private static int[] sort(int[] array){
+        // 处理边界条件
+        if (array.length == 0 || array.length == 1) {
+            return array;
+        }
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < array.length-j-1; i++) {
+                if (array[i]>array[i+1]) {
+                    swap(array, i, i+1);
+                }
+            }
+        }
+        return array;
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 1, 4, 10, 6, 3, 1, 8, 9, 5, 3};
+        for (int j : sort(array)) {
+            System.out.print(j + " ");
+        }
+    }
+}
