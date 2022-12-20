fc460fc90ab3a8f8832087a9f33341e6a35dba06
huabin <1184884206@qq.com>
2022-12-20 12:07:38 +0800

    面试题18：树的子结构

---
+++ src/com/huabin/common/TreeNode.java
@@ -0,0 +1,16 @@
+package com.huabin.common;
+
+/**
+ * @Author huabin
+ * @DateTime 2022-12-20 11:14
+ * @Desc 二叉树节点
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) { val = x; }
+
+}
