25fd22eb662fb31478b7a5da6421694d9b26dffe
huabin <1184884206@qq.com>
2023-08-23 18:03:34 +0800

    翻转字符串里的单词&左旋转字符串

---
+++ src/com/huabin/leetcode/editor/cn/ZuoXuanZhuanZiFuChuanLcof.java
@@ -0,0 +1,58 @@
+//字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数
+//将返回左旋转两位得到的结果"cdefgab"。
+//
+//
+//
+// 示例 1：
+//
+// 输入: s = "abcdefg", k = 2
+//输出: "cdefgab"
+//
+//
+// 示例 2：
+//
+// 输入: s = "lrloseumgh", k = 6
+//输出: "umghlrlose"
+//
+//
+//
+//
+// 限制：
+//
+//
+// 1 <= k < s.length <= 10000
+//
+//
+// Related Topics 数学 双指针 字符串 👍 439 👎 0
+
+
+package com.huabin.leetcode.editor.cn;
+public class ZuoXuanZhuanZiFuChuanLcof{
+    public static void main(String[] args) {
+        Solution solution = new ZuoXuanZhuanZiFuChuanLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String reverseLeftWords(String s, int n) {
+            // 分成三步：1、反转前n 2、反转剩余 3、全部反转
+            int len = s.length();
+            StringBuilder sb = new StringBuilder(s);
+            reverse(sb, 0, n-1);
+            reverse(sb, n, len-1);
+            reverse(sb, 0, len-1);
+            return sb.toString();
+        }
+
+        private void reverse(StringBuilder sb, int start, int end){
+            while (start < end) {
+                char temp = sb.charAt(start);
+                sb.setCharAt(start, sb.charAt(end));
+                sb.setCharAt(end, temp);
+                start++;
+                end--;
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
