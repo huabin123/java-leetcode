6fd0eed0b114fe0b4f426ec1d0c116a8f0b596e5
huabin <1184884206@qq.com>
2023-08-30 11:07:58 +0800

    二叉树的右视图

---
+++ src/com/huabin/leetcode/editor/cn/BinaryTreeRightSideView.java
@@ -0,0 +1,98 @@
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+//
+//
+// 示例 2:
+//
+//
+//输入: [1,null,3]
+//输出: [1,3]
+//
+//
+// 示例 3:
+//
+//
+//输入: []
+//输出: []
+//
+//
+//
+//
+// 提示:
+//
+//
+// 二叉树的节点个数的范围是 [0,100]
+//
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 932 👎 0
+
+
+package com.huabin.leetcode.editor.cn;
+
+import com.huabin.common.TreeNode;
+
+import java.util.*;
+
+public class BinaryTreeRightSideView{
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeRightSideView().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            // 思路：层序遍历的时候判断这一层是否遍历到最后一个
+            List<Integer> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> list = new ArrayList<>();
+                TreeNode cur = new TreeNode();
+                while (size > 0) {
+                    TreeNode node = queue.poll();
+                    cur = node;
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    size--;
+                }
+                res.add(cur.val);
+            }
+            return res;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
