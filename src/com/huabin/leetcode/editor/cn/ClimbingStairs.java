28978b668a1538ee2cbca50d79603fc52d0717fc
huabin <1184884206@qq.com>
2023-09-20 16:42:24 +0800

    爬楼梯（完全背包版）

--- src/com/huabin/leetcode/editor/cn/ClimbingStairs.java
+++ src/com/huabin/leetcode/editor/cn/ClimbingStairs.java
@@ -41,20 +41,44 @@ public class ClimbingStairs{
         Solution solution = new ClimbingStairs().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
+//    class Solution {
+//        public int climbStairs(int n) {
+//            if (n <= 1) {
+//                return n;
+//            }
+//            int[] dp = new int[n + 1];
+//            dp[1] = 1;
+//            dp[2] = 2;
+//            for (int i = 3; i <= n; i++) {
+//                dp[i] = dp[i - 1] + dp[i - 2];
+//            }
+//            return dp[n];
+//        }
+//    }
+
+    /**
+     * 这个题还能稍作修改能转化成一个完全背包问题
+     * 有n阶台阶，一次可以上m阶，问跳到顶楼有几种方法（排列）
+     */
     class Solution {
         public int climbStairs(int n) {
-            if (n <= 1) {
-                return n;
-            }
             int[] dp = new int[n + 1];
-            dp[1] = 1;
-            dp[2] = 2;
-            for (int i = 3; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
+            int m = 2; //有兩個物品：itme1重量爲一，item2重量爲二
+            dp[0] = 1;
+
+            for (int i = 1; i <= n; i++) { // 遍历背包
+                for (int j = 1; j <= m; j++) { //遍历物品
+                    if (i >= j)  //當前的背包容量 大於 物品重量的時候，我們才需要記錄當前的這個裝得方法（方法數+）
+                        dp[i] += dp[i - j];
+                }
             }
+
             return dp[n];
         }
     }
+
+
+
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
