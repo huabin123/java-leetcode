2ef45800b8b248303329d3b444fefd5c5275cccf
huabin <1184884206@qq.com>
2023-09-22 14:17:53 +0800

    122.买卖股票的最佳时机II(动态规划)

--- src/com/huabin/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
+++ src/com/huabin/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
@@ -50,12 +50,31 @@ public class BestTimeToBuyAndSellStockIi{
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 贪心，把利润为正数的天数加起来就行了
-        public int maxProfit(int[] prices) {
+        public int maxProfit1(int[] prices) {
             int sum = 0;
             for (int i = 1; i < prices.length; i++) {
                 sum += Math.max(prices[i] - prices[i - 1], 0);
             }
             return sum;
+        }
+
+        /**
+         * 动规，基本上和121题类似，不同的点在于本题可以反复买卖
+         *
+         * 在121. 买卖股票的最佳时机 (opens new window)中，因为股票全程只能买卖一次，所以如果买入股票，那么第i天持有股票即dp[i][0]一定就是 -prices[i]。
+         * 而本题，因为一只股票可以买卖多次，所以当第i天买入股票的时候，所持有的现金可能有之前买卖过的利润。
+         * 那么第i天持有股票即dp[i][0]，如果是第i天买入股票，所得现金就是昨天不持有股票的所得现金 减去 今天的股票价格 即：dp[i - 1][1] - prices[i]。
+         */
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int[][] dp = new int[n][2];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+            for (int i = 1; i < n; ++i) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);    //注意这里是和121. 买卖股票的最佳时机唯一不同的地方。
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[n - 1][0];    // 卖出股票收益高于持有股票收益，因此取[0]
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
