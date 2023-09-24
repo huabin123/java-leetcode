7c6e12443bd459db07878cb6a2bd425d50e6a9e6
huabin <1184884206@qq.com>
2023-09-24 11:01:17 +0800

    300.最长递增子序列

--- src/com/huabin/leetcode/editor/cn/LongestIncreasingSubsequence.java
+++ src/com/huabin/leetcode/editor/cn/LongestIncreasingSubsequence.java
@@ -46,14 +46,33 @@
 
 
 package com.huabin.leetcode.editor.cn;
+
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence{
     public static void main(String[] args) {
         Solution solution = new LongestIncreasingSubsequence().new Solution();
+        int[] nums = new int[]{0};
+        System.out.println(solution.lengthOfLIS(nums));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lengthOfLIS(int[] nums) {
-
+            if (nums.length <= 1) {
+                return nums.length;
+            }
+            int[] dp = new int[nums.length];
+            int res = 0;
+            Arrays.fill(dp, 1);
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                    res = Math.max(res, dp[i]);
+                }
+            }
+            return res;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
